a4918c06528f52f487aebd9033be8b5a5b250eea
Nico Hämäläinen <nico@endwater.local>
2017-05-17 21:56:48 +0300

    Initial commit

---
+++ realm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  realm
+//
+//  Created by Nico Hämäläinen on 17/05/2017.
+//  Copyright © 2017 Nico Hämäläinen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
